2f36eb64a8ff4ba7eff6a16d0dfd1f6ea3d265d7
LaurenceLiZhixin <382673304@qq.com>
2021-03-04 10:45:00 +0800

    init helloworld

--- prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -O /home/shell/nacos-server-1.3.2.tar.gz https://handson.oss-cn-shanghai.aliyuncs.com/nacos-server-1.3.2.tar.gz
-
-tar -xzvf /home/shell/nacos-server-1.3.2.tar.gz -C /home/shell/
-
-sh /home/shell/nacos/bin/startup.sh -m standalone
