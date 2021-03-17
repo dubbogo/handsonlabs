4f9e4f1890da16c4902d12a770d58ff3db074a41
LaurenceLiZhixin <382673304@qq.com>
2021-03-17 10:46:37 +0800

    fix: add libssl tool

--- tutorials/helloworld/prepare.sh
+++ tutorials/helloworld/prepare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install libssl-dev
+
 wget -O /home/shell/nacos-server-1.3.2.tar.gz https://handson.oss-cn-shanghai.aliyuncs.com/nacos-server-1.3.2.tar.gz
 
 tar -xzvf /home/shell/nacos-server-1.3.2.tar.gz -C /home/shell/
